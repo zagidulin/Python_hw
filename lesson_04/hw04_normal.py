73eeafa72598227c0398ad934f0f5fcae5444e61
zagidulin <tura1@bk.ru>
2019-08-30 01:28:48 +0300

    задания к уроку 4

---
+++ lesson_04/hw04_normal.py
@@ -0,0 +1,134 @@
+# Задание-1:
+# Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью re и без.
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
+       'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
+       'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
+       'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV'\
+       'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW'\
+       'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC'\
+       'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR'\
+       'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm'\
+       'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn'\
+       'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS'\
+       'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf'\
+       'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH'\
+       'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
+       'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
+       'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+#РЕШЕНИЕ БЕЗ re
+res_line = []
+a_z = 'abcdefghijklmnopqrstuvwxyz'
+count = 0
+for i in line:
+    if i in a_z:
+        try:
+            res_line[count] += i
+        except IndexError:
+            res_line.append(i)
+        continue
+    else:
+        try:
+            if any(j in a_z for j in res_line[count]):
+                count = len(res_line)
+                continue
+        except IndexError:
+            continue
+if len(res_line) == 1:
+# для проверки того, что исходная строка не состоит из одних строчных букв
+    res_line.pop(0)
+print(res_line)
+
+#РЕШЕНИЕ C re
+
+import re
+pattern = r"[a-z]+(?=[A-Z]+[a-z])|(?<=[A-Z])[a-z]+"
+result = re.findall(pattern, line)
+if len(result) == 1:
+# для проверки того, что исходная строка не состоит из одних строчных букв
+       result.pop(0)
+print(result)
+
+# Задание-2:
+# Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки 
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
+       'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
+       'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'\
+       'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV'\
+       'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW'\
+       'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC'\
+       'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR'\
+       'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm'\
+       'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn'\
+       'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS'\
+       'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf'\
+       'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH'\
+       'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'\
+       'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
+       'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+
+#РЕШЕНИЕ БЕЗ re
+res_line_2 = []
+a_z = 'abcdefghijklmnopqrstuvwxyz'
+A_Z = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+count_2 = 0
+while count_2 < len(line_2):
+  if line_2[count_2] in a_z:
+    exit = 0
+    j = 1
+    count_2 += 1
+    while count_2 < len(line_2) and exit == 0:
+      if line_2[count_2] in a_z:
+        j += 1
+        count_2 += 1
+      elif j >= 2:
+        k = 1
+        while count_2 < len(line_2) and exit == 0:
+          if line_2[count_2] in A_Z:
+            count_2 += 1
+            k += 1
+          elif k > 3:
+            res_line_2.append(line_2[count_2-k+1:count_2-2])
+            exit = 1
+          else:
+            exit = 1
+      else:
+        exit = 1
+  else:
+    count_2 += 1
+print(res_line_2)
+
+#РЕШЕНИЕ C re
+pattern_2 = r"[a-z]{2}([A-Z]+)[A-Z]{2}"
+result_2 = re.findall(pattern_2, line_2)
+print(result_2)
+
+
+# Задание-3:
+# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
+# произвольными целыми цифрами, в результате в файле должно быть
+# 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр
+# в вышезаполненном файле.
+
+import random
+
+lst = [random.randint(0, 9) for _ in range(2500)]
+lst = ''.join(list(map(lambda x: str(x), lst)))
+print(len(lst))
+
+my_file = open('Num.txt', 'w', encoding='UTF-8')
+my_file.write(lst)
+my_file.close()
+
+# остальную часть задания доделаю к следующему уроку
