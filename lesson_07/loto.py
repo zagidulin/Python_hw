5395ea8ddf09b4314f6b3a4065286d94351f3125
zagidulin <tura1@bk.ru>
2019-09-12 22:00:36 +0300

    Выполнил задание к уроку 7 (лото)

--- lesson_07/loto.py
+++ lesson_07/loto.py
@@ -1,59 +1,98 @@
-﻿#!/usr/bin/python3
+﻿import random
 
-"""
-== Лото ==
+class Box:
+    def __init__(self):
+        self.barrels = [_ for _ in range (1, 91)]
 
-Правила игры в лото.
+class Card:
+    def __init__(self, who):
+        self.who = who
+        self.card_blank = [['' for _ in range(9)] for _ in range(3)]
+        self.card_nums = random.sample(range(1, 91), 15)
+    def get_card(self):
+        for j in range(3):
+            index_ = random.sample(range(8), 5)
+            for k in range(5):
+                self.card_blank[j][index_[k]] = self.card_nums[0]
+                self.card_nums.pop(0)
+    def __str__(self):
+        print(f'Карточка {self.who+"а"}'.center(25, "-"))
+        for j in self.card_blank:
+            for k in j:
+                print(f'{k}'.rjust(2), end = " ")
+            print()
+        return "-"*25
+    def card_check(self, n):
+        for j in range(3):
+            if n in self.card_blank[j]:
+                self.card_blank[j][self.card_blank[j].index(n)] = "X"
+                return True
+                break
+            else:
+                continue
+            return False
 
-Игра ведется с помощью специальных карточек, на которых отмечены числа, 
-и фишек (бочонков) с цифрами.
+class Game:
+    def __init__(self, **kwargs):
+        pass
 
-Количество бочонков — 90 штук (с цифрами от 1 до 90).
+    def start_game(self):
+        box = Box()
+        card_p = Card("Игрок")
+        card_c = Card("Компьютер")
+        card_p.get_card()
+        card_c.get_card()
 
-Каждая карточка содержит 3 строки по 9 клеток. В каждой строке по 5 случайных цифр, 
-расположенных по возрастанию. Все цифры в карточке уникальны. Пример карточки:
+        luck_p = luck_c = 0
+        game = 1
 
---------------------------
-    9 43 62          74 90
- 2    27    75 78    82
-   41 56 63     76      86 
---------------------------
+        while len(box.barrels) > 0 and game == 1:
+            print(card_p)
+            print(card_c)
+            n = random.choice(box.barrels)
+            box.barrels.remove(n)
+            print("Номер бочонка: ", n)
+            print(len(box.barrels), "- осталось бочонков")
+            answer = input("Вычеркнуть номер из карточки Игрока? (y/n)\n")
+            while True:
+                if answer == "y":
+                    if card_p.card_check(n):
+                        luck_p += 1
+                        break
+                    else:
+                        print("Такого номера нет в карточке Игрока.\nИгра окончена. Вы проиграли")
+                        game = 0
+                        break
+                elif answer == "n":
+                    if card_p.card_check(n):
+                        print("Такой номер есть в карточке Игрока.\nИгра окончена. Вы проиграли")
+                        game = 0
+                        break
+                    else:
+                        break
+                else:
+                    answer = input("Введен некорректный ответ.\nВычеркнуть номер из карточки Игрока? (y/n)\n")
+                    continue
 
-В игре 2 игрока: пользователь и компьютер. Каждому в начале выдается 
-случайная карточка. 
+            if card_c.card_check(n):
+                luck_c += 1
+            else:
+                pass
 
-Каждый ход выбирается один случайный бочонок и выводится на экран.
-Также выводятся карточка игрока и карточка компьютера.
+            if luck_p == 15:
+                if luck_c == 15:
+                    print("Ничья")
+                    game = 0
+                else:
+                    print(card_p)
+                    print("Вы победили!")
+                    game = 0
+            elif luck_c == 15:
+                print(card_c)
+                print("Вы проиграли!")
+                game = 0
+            else:
+                continue
 
-Пользователю предлагается зачеркнуть цифру на карточке или продолжить.
-Если игрок выбрал "зачеркнуть":
-	Если цифра есть на карточке - она зачеркивается и игра продолжается.
-	Если цифры на карточке нет - игрок проигрывает и игра завершается.
-Если игрок выбрал "продолжить":
-	Если цифра есть на карточке - игрок проигрывает и игра завершается.
-	Если цифры на карточке нет - игра продолжается.
-	
-Побеждает тот, кто первый закроет все числа на своей карточке.
-
-Пример одного хода:
-
-Новый бочонок: 70 (осталось 76)
------- Ваша карточка -----
- 6  7          49    57 58
-   14 26     -    78    85
-23 33    38    48    71   
---------------------------
--- Карточка компьютера ---
- 7 11     - 14    87      
-      16 49    55 77    88    
-   15 20     -       76  -
---------------------------
-Зачеркнуть цифру? (y/n)
-
-Подсказка: каждый следующий случайный бочонок из мешка удобно получать 
-с помощью функции-генератора.
-
-Подсказка: для работы с псевдослучайными числами удобно использовать 
-модуль random: http://docs.python.org/3/library/random.html
-
-"""
+play = Game()
+play.start_game()
